597fad0e4ddb30390394e6158f518df1754ca4e1
robotlyb <robotlyb@hit.edu.cn>
2013-11-11 16:11:20 +0800

    创建application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GetAddress::Application.config.secret_token = '3f0f868be3bf37e82ef94d6e308d8499c4eb7fc46a21d8c725e4af91e81b6dc57308abd195fff184c0c36884143485d174d36e3889785a6dbc96e268ec576a32'
